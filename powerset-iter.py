86feb9b34a5dc461d65e24f65006a9dcf4f41d5e
emallove <emallove@yahoo.com>
2021-07-05 18:30:46 +0000

    Solution works on list of length 5
    solution works on list of length 5, but fails 29/32 hidden tests on
    hackerrank.com.
    
    $ time python3 powerset-iter.py
    seq_size =  18
             6302940 function calls in 6.719 seconds
    
       Ordered by: standard name
    
       ncalls  tottime  percall  cumtime  percall filename:lineno(function)
            1    0.013    0.013    6.719    6.719 <string>:1(<module>)
       262144    0.122    0.000    0.140    0.000 powerset-iter.py:115(contains_an_adjacent_pair)
            1    0.000    0.000    0.000    0.000 powerset-iter.py:123(getSeq)
       260505    0.107    0.000    0.107    0.000 powerset-iter.py:131(getSubSet)
            1    0.175    0.175    6.706    6.706 powerset-iter.py:139(maxSubsetSum)
      4718592    5.220    0.000    5.220    0.000 powerset-iter.py:80(contains_adj_set_bits)
            1    0.981    0.981    6.224    6.224 powerset-iter.py:87(get_power_set)
            1    0.000    0.000    6.719    6.719 {built-in method builtins.exec}
       262146    0.018    0.000    0.018    0.000 {built-in method builtins.len}
       260505    0.039    0.000    0.039    0.000 {built-in method builtins.sum}
            1    0.000    0.000    0.000    0.000 {built-in method math.pow}
       539041    0.045    0.000    0.045    0.000 {method 'append' of 'list' objects}
            1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
    
    v =  347
    
    real    0m6.771s
    user    0m6.737s
    sys     0m0.031s

--- powerset-iter.py
+++ powerset-iter.py
@@ -1,12 +1,96 @@
 # python3 program for power set
 
+# 
+# 
+# Given an array of integers, find the subset of non-adjacent elements with the
+# maximum sum. Calculate the sum of that subset. It is possible that the maximum
+# sum is , the case when all elements are negative.
+# 
+# Example
+# 
+# The following subsets with more than  element exist. These exclude the empty
+# subset and single element subsets which are also valid.
+# 
+# Subset      Sum
+# [-2, 3, 5]   6
+# [-2, 3]      1
+# [-2, -4]    -6
+# [-2, 5]      3
+# [1, -4]     -3
+# [1, 5]       6
+# [3, 5]       8 The maximum subset sum is . Note that any individual element is
+# a subset as well.
+# 
+# 
+# In this case, it is best to choose no element: return .
+# 
+# Function Description
+# 
+# Complete the  function in the editor below.
+# 
+# maxSubsetSum has the following parameter(s):
+# 
+# int arr[n]: an array of integers
+# Returns
+# - int: the maximum subset sum
+# 
+# Input Format
+# 
+# The first line contains an integer, .
+# The second line contains  space-separated integers .
+# 
+# Constraints
+# 
+# Sample Input 0
+# 
+# 5
+# 3 7 4 6 5
+# Sample Output 0
+# 
+# 13
+# Explanation 0
+# 
+# Our possible subsets are  and . The largest subset sum is  from subset 
+# 
+# Sample Input 1
+# 
+# 5
+# 2 1 5 8 4
+# Sample Output 1
+# 
+# 11
+# Explanation 1
+# 
+# Our subsets are  and . The maximum subset sum is  from the first subset listed.
+# 
+# Sample Input 2
+# 
+# 5
+# 3 5 -7 8 10
+# Sample Output 2
+# 
+# 15
+# Explanation 2
+# 
+# Our subsets are  and . The maximum subset sum is  from the fifth subset listed.
+
 import math
 
-def printPowerSet(set,set_size):
-  
+# Return true if the bit string contains adjacent set bits
+def contains_adj_set_bits(b, bits):
+  for i in range(bits):
+    if ((b & (1 << i)) and (b & (i << (i+1)))):
+      return True
+
+  return False
+
+def get_power_set(my_set):
+
+  set_size = len(my_set)
+
   powerSet = []
 
-  # set_size of power set of a set
+  # set_size of power my_set of a my_set
   # with set_size n is (2**n -1)
   pow_set_size = (int) (math.pow(2, set_size))
   counter = 0
@@ -16,51 +100,75 @@ def printPowerSet(set,set_size):
   for counter in range(0, pow_set_size):
     subSet = []
     for j in range(0, set_size):
-  
-      
-      # Check if jth bit in the
-      # counter is set If set then
-      # print jth element from set
-      if((counter & (1 << j)) > 0):
-        subSet.append(set[j])
+
+      # Check if jth bit in the counter is set, then get jth element from
+      # my_set.
+      if not contains_adj_set_bits(counter, set_size):
+        if ((counter & (1 << j)) > 0):
+          subSet.append(my_set[j])
+
     powerSet.append(subSet)
 
   return powerSet
 
-# Driver program to test printPowerSet
-#set = ['a', 'b', 'c', 'd', 'e', 'f']
-
 # If there's an adj pair mark invalid
-def is_invalid(L):
-  print("is invalid, L = ", L)
+def contains_an_adjacent_pair(L):
 
-  for i in range(len(L)-1):
-
-    print("L[i] = ", L[i])
-    print("L[i+1] = ", L[i+1])
-
+  for i in range(len(L) - 1):
     if L[i]+1 == L[i+1]:
       return True
 
   return False
 
+def getSeq(v):
+  r = []
+  for i in range(v):
+    r.append(i)
+  return r
 
-set = [1,2,3,4,5,6]
+# Get sub-set of sets based on indicies,
+# e.g., [0,2,4] of [0,1,2,3,4,5] returns [0,2,4]
+def getSubSet(indicies, sets):
 
-v = printPowerSet(set, 6)
+  r = []
+  for i in indicies:
+    r.append(sets[i])
 
-print("len(v) = ", len(v))
+  return r
 
-v2 = []
-for i in range(len(v) - 1):
-  print("i = ", i)
-  if is_invalid(v[i]):
-    continue
-  v2.append(v[i])
+def maxSubsetSum(mainSet):
 
-print("len(v) = ", len(v))
+  indiciesPowerSet = getSeq(len(mainSet))
+  indiciesPowerSet = get_power_set(indiciesPowerSet)
 
-for s in v2:
-    print(*s)
+  acceptableSets = []
 
-# This code is contributed by mits.
+  # Remove pairs which contain an adjacency
+  for ithSubSet in indiciesPowerSet:
+
+    if contains_an_adjacent_pair(ithSubSet):
+      continue
+
+    subset = getSubSet(ithSubSet, mainSet)
+    acceptableSets.append(subset)
+
+  # Find the max of the valid sub-lists
+  _max = 0
+  for s in acceptableSets:
+      _sum = sum(s)
+      if _sum > _max:
+        _max = _sum
+
+  return _max
+
+
+sample_input_2 = [3,5,-7,8,10]
+random_org_sample = [38, 12, 52, 93, 82, 21, 25, 71, 87, 98, 28, 89, 65, 86, 22, 15, 23, 14]
+
+seq_size = len(random_org_sample)
+print("seq_size = ", seq_size)
+
+import cProfile
+cProfile.run('v = maxSubsetSum(random_org_sample)')
+#v = maxSubsetSum(random_org_sample)
+print("v = ", v)
